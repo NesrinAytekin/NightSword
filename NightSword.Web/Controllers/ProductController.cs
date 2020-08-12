c2fb68c37b2067b07eeb965690a0a894f2c3d1d3
NesrinAytekin <NesrinAytekin@users.noreply.github.com>
2020-08-12 14:00:20 +0300

    Product Controller Added

--- NightSword.Web/Controllers/ProductController.cs
+++ NightSword.Web/Controllers/ProductController.cs
@@ -12,24 +12,26 @@ namespace NightSword.Web.Controllers
     {
 
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, ICategoryService categoryService)
         {
             this._productService = productService;
+            this._categoryService = categoryService;
         }
         public IActionResult Index()
         {
             return View(_productService.GetProducts().OrderBy(x=>x.Name).ToList());
         }
 
-        public IActionResult ProductsByCategory(string categorySlug)
+        public IActionResult ProductsByCategory(int categoryId)
         {
-            CategoryDto category =  _productService.GetCategories().Where(x => x.Slug == categorySlug).FirstOrDefault();
+            CategoryDto category =  _productService.GetCategories().Where(x => x.Id == categoryId).FirstOrDefault();
 
             if (category == null) return RedirectToAction("Index");
 
             ViewBag.CategoryName = category.Name;
-            ViewBag.CategorySlug = categorySlug;
+            ViewBag.CategorySlug = categoryId;
 
             return View(_productService.GetProducts().Where(x => x.CategoryId == category.Id).ToList());
         }
